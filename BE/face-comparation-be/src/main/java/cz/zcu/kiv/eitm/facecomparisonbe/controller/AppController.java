35e8049206f47793ceb75d3df3bbab0f11ea07f5
Ondřej Váně <onry.vane@gmail.com>
2021-04-05 18:46:01 +0200

    Úprava cross originu

--- BE/face-comparation-be/src/main/java/cz/zcu/kiv/eitm/facecomparisonbe/controller/AppController.java
+++ BE/face-comparation-be/src/main/java/cz/zcu/kiv/eitm/facecomparisonbe/controller/AppController.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "${client.url}")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class AppController {
 
