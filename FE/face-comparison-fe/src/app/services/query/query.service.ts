358c9bbdc47be0521e283d918440c31360b19690
Ondřej Váně <onry.vane@gmail.com>
2021-04-05 20:41:09 +0200

    Oprava url

--- FE/face-comparison-fe/src/app/services/query/query.service.ts
+++ FE/face-comparison-fe/src/app/services/query/query.service.ts
@@ -18,7 +18,7 @@ const httpOptions = {
 })
 export class QueryService {
 
-  backendUrl = 'http://147.228.173.44:8080/';
+  backendUrl = 'http://localhost:8080/';
   loginUrl = 'login';
   registerUrl = 'register';
   testBackendUrl = 'test';
